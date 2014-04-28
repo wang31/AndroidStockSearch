ef0bf56d5a4eac67a2da9afde190080b49c29e32
ZWang <hyopalexus@gmail.com>
2014-04-27 20:42:00 -0700

    just commiting everything

--- gen/com/cs571/zhengwang/R.java
+++ gen/com/cs571/zhengwang/R.java
@@ -1091,13 +1091,13 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003f;
+        public static final int action_settings=0x7f050045;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
-        public static final int container=0x7f05003c;
+        public static final int container=0x7f05003d;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
@@ -1106,16 +1106,22 @@ containing a value of this type.
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
+        public static final int fbbutton=0x7f050043;
+        public static final int fragment_place=0x7f05003f;
+        public static final int fragmentonell=0x7f050040;
+        public static final int headlinesview=0x7f05003c;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
-        public static final int input=0x7f05003d;
+        public static final int input=0x7f05003e;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
+        public static final int newsbutton=0x7f050042;
+        public static final int newsview=0x7f050044;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
         public static final int progress_circular=0x7f050018;
@@ -1138,11 +1144,11 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
-        public static final int textview=0x7f05003e;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
+        public static final int webview=0x7f050041;
         public static final int withText=0x7f05000c;
     }
     public static final class integer {
@@ -1195,14 +1201,21 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
-        public static final int activity_main=0x7f030018;
-        public static final int fragment_main=0x7f030019;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
+        public static final int activity_headlines=0x7f030018;
+        public static final int activity_main=0x7f030019;
+        public static final int fragment_headlines=0x7f03001a;
+        public static final int fragment_main=0x7f03001b;
+        public static final int fragment_one=0x7f03001c;
+        public static final int fragment_two=0x7f03001d;
+        public static final int list_titles=0x7f03001e;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001f;
     }
     public static final class menu {
-        public static final int main=0x7f0c0000;
+        public static final int headlines=0x7f0c0000;
+        public static final int main=0x7f0c0001;
     }
     public static final class string {
+        public static final int Chart=0x7f0a0012;
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
          */
         public static final int abc_action_bar_home_description=0x7f0a0001;
@@ -1246,8 +1259,12 @@ containing a value of this type.
         public static final int action_settings=0x7f0a000e;
         public static final int app_name=0x7f0a000d;
         public static final int button_query=0x7f0a0010;
+        public static final int fbbutton=0x7f0a0014;
+        public static final int hello_world=0x7f0a0016;
         public static final int input=0x7f0a000f;
+        public static final int newsbutton=0x7f0a0013;
         public static final int title=0x7f0a0011;
+        public static final int title_activity_headlines=0x7f0a0015;
     }
     public static final class style {
         /** 
